e60e47a83f2c86f11adb3b0c5669303867e9bb5f
RTae <potae02@gmail.com>
2021-02-15 13:32:37 +0700

    Update main.c

--- week4/LAB/main.c
+++ week4/LAB/main.c
@@ -5,6 +5,7 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define CS PB2
 #define CS_DDR DDB2
@@ -33,36 +34,52 @@ void SPI_Init()
 {   
     /* set MOSI CLK CS as Output*/
     DDRB |= (1 << CS_DDR) | (1 << CLK) | (1 << MOSI);
+    // Chip select high
+    PORTB |= (1 << CS);
+    // Chip select low
+    PORTB &= ~(1 << CS);
     /* Enable SPI, Master mode, clk/16 */
     SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
 }
 
 uint16_t SPI_READ()
 {
-    uint8_t rx_byte;
-    uint16_t rx_12bits;
+    uint16_t high_byte;
+    uint16_t low_byte;
+    uint16_t out_12bits;
     
-    PORTB &= ~(1 << CS);                        // Chip select low
+    PORTB &= ~(1 << CS);                                            // Chip select low
 
-    SPDR = 0xFF;                                // put dummy byte in SPDR
+    SPDR = 0xFF;                                                    // put dummy byte in SPDR
 
-    while(!(SPSR & (1<<SPIF)));                 // wait for SPIF high 
-
-    rx_byte = SPDR & 0b00111111;                  // copy SPDR out
+    while(!(SPSR & (1<<SPIF)));                                     // wait for SPIF high 
     
-    rx_12bits = rx_byte << 7;
+    /*xx0[B11][B10][B9][B8][B7]*/
+    high_byte = SPDR;                                               // copy SPDR out
+        
+    SPDR = 0xFF;                                                    // put dummy byte in SPDR
+
+    while(!(SPSR & (1<<SPIF)));                                     // wait for SPIF high     
     
-    SPDR = 0xFF;                                // put dummy byte in SPDR
-
-    while(!(SPSR & (1<<SPIF)));                 // wait for SPIF high     
-
-    rx_byte = SPDR >>= 1;                         // copy SPDR out
-
-    rx_12bits |= rx_byte;                             // Concat bit
+    /*[B6][B5][B4][B3][B2][B1][B0][B1]*/
+    low_byte = SPDR;                                                // copy SPDR out
     
-    PORTB |= (1 << CS);                         // Chip select high
+    /*xx0[B11][B10][B9][B8][B7] 0   0   0   0   0   0   0    0 */
+    /*                                                      OR */
+    /*000 0     0    0   0  0  [B6][B5][B4][B3][B2][B1][B0][B1]*/
+    /*---------------------------------------------------------*/
+    /*xx0[B11][B10][B9][B8][B7][B6][B5][B4][B3][B2][B1][B0][B1]*/
+    out_12bits = (high_byte << 8) | low_byte;                       // Concatenate bit
+    
+    /*[B11][B10][B9][B8][B7][B6][B5][B4][B3][B2][B1][B0][B1]000*/
+    out_12bits <<= 3;                                               // Shift left 3
+    
+    /*0000[B11][B10][B9][B8][B7][B6][B5][B4][B3][B2][B1][B0]*/
+    out_12bits >>= 4;                                               // Shift right 4
+    
+    PORTB |= (1 << CS);                                             // Chip select high
 
-    return rx_12bits;
+    return out_12bits;
 }
 
 int main(void) {
@@ -71,17 +88,17 @@ int main(void) {
     SPI_Init();
 
     uint16_t sensor;
-    uint16_t temp;
-    unsigned char text[] = "Temp = ";
+    float temp;
+    unsigned char text[] = "Temperature = ";
     unsigned char buffer[10];
 
     while (1) {
         sensor = SPI_READ();             // Read data from sensor
         
-        temp = ((sensor/4095.0) * 5.0 - 0.5) * 100.0;
+        // temp = (((sensor/4096.0) * 5.0) - 0.5) * 100.0 ;
         
-        sprintf(buffer,"%u",sensor);     // convert to string
-        strcat(buffer, "\n");
+        dtostrf(temp, 3, 2, buffer);
+        strcat(buffer, " °C\n");
         
         print(text);
         print(buffer);
